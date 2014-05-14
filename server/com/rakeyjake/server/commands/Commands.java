40591d53156b6c5b003e227b1ac89a972c28a728
Jake Arnold <rakeyjake@hotmail.co.uk>
2014-05-14 21:10:33 +0100

    test

--- server/com/rakeyjake/server/commands/Commands.java
+++ server/com/rakeyjake/server/commands/Commands.java
@@ -54,6 +54,8 @@ public class Commands implements PacketType {
 	}
 
 	public static void ownerCommands(Client c, String playerCommand) {
+		
+		System.out.println("this is a etest");
 		/*
 		 * Owner commands
 		 */
